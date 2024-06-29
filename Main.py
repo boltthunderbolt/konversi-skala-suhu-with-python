2fb43ed751f04ce10031348d8045a433999446d3
Thunder Bolt <bolt.thunderbolt168@gmail.com>
2024-06-29 18:04:16 +0700

    Mengubah tempat daftar satuan skala ke dalam array, menambahkan validasi input user saat memasukkan satuan skala, mengubah di bagian function konversi_skala_suhu di bagian mencari nilai konversi, dan terakhir mengubah hasil konversi sesuai dengan yang sudah diubah di function sebelumnya

--- Main.py
+++ Main.py
@@ -34,7 +34,7 @@ skala_yang_ingin_dikonversi = skala_yang_ingin_dikonversi - 1;
 print(f'Konversikan ke: {list_satuan_skala[skala_yang_ingin_dikonversi]}')
 
 # Validasi input
-if skala_yang_dipilih not in list_satuan_skala and skala_yang_ingin_dikonversi not in list_satuan_skala:
+if skala_yang_dipilih < 0 or skala_yang_dipilih >= len(list_satuan_skala) or skala_yang_ingin_dikonversi < 0 or skala_yang_ingin_dikonversi >= len(list_satuan_skala):
   #! Jika skala_yang_dipilih dan skala_yang_dikonversi tidak ada dalam daftar list_satuan_skala maka error ditampilkan
   raise ValueError('Satuan skala yang dimasukkan tidak valid. Harap masukkan dengan benar');
 
@@ -46,174 +46,174 @@ def konversi_skala_suhu(skala_yang_dipilih, nilai_suhu, skala_yang_ingin_dikonve
       return nilai_suhu;
     elif skala_yang_ingin_dikonversi == list_satuan_skala[1]:
       return nilai_suhu * 9/5 - 459.67;
-    elif skala_yang_ingin_dikonversi == 'celcius':
+    elif skala_yang_ingin_dikonversi == list_satuan_skala[2]:
       return nilai_suhu - 273.15;
-    elif skala_yang_ingin_dikonversi == 'reamur':
+    elif skala_yang_ingin_dikonversi == list_satuan_skala[3]:
       return (nilai_suhu - 273.15) * 4/5;
-    elif skala_yang_ingin_dikonversi == 'rankine':
+    elif skala_yang_ingin_dikonversi == list_satuan_skala[4]:
       return nilai_suhu * 9/5;
-    elif skala_yang_ingin_dikonversi == 'delisle':
+    elif skala_yang_ingin_dikonversi == list_satuan_skala[5]:
       return (373.15 - nilai_suhu) * 3/2;
-    elif skala_yang_ingin_dikonversi == 'newton':
+    elif skala_yang_ingin_dikonversi == list_satuan_skala[6]:
       return (nilai_suhu - 273.15) * 33/100;
-    elif skala_yang_ingin_dikonversi == 'romer':
+    elif skala_yang_ingin_dikonversi == list_satuan_skala[7]:
       return (nilai_suhu - 273.15) * 21/40 + 7.5;
     else:
       return;
 
   # Mencari nilai konversi dari satuan Fahrenheit
-  elif skala_yang_dipilih == 'fahrenheit':
-    if skala_yang_ingin_dikonversi == 'kelvin':
+  elif skala_yang_dipilih == list_satuan_skala[1]:
+    if skala_yang_ingin_dikonversi == list_satuan_skala[0]:
       return (nilai_suhu + 459.67) * 5/9;
-    elif skala_yang_ingin_dikonversi == 'fahrenheit':
+    elif skala_yang_ingin_dikonversi == list_satuan_skala[1]:
       return nilai_suhu;
-    elif skala_yang_ingin_dikonversi == 'celcius':
+    elif skala_yang_ingin_dikonversi == list_satuan_skala[2]:
       return (nilai_suhu - 32) * 5/9;
-    elif skala_yang_ingin_dikonversi == 'reamur':
+    elif skala_yang_ingin_dikonversi == list_satuan_skala[3]:
       return (nilai_suhu - 32) * 4/9;
-    elif skala_yang_ingin_dikonversi == 'rankine':
+    elif skala_yang_ingin_dikonversi == list_satuan_skala[4]:
       return nilai_suhu + 459.67;
-    elif skala_yang_ingin_dikonversi == 'delisle':
+    elif skala_yang_ingin_dikonversi == list_satuan_skala[5]:
       return (212 - nilai_suhu) * 5/6;
-    elif skala_yang_ingin_dikonversi == 'newton':
+    elif skala_yang_ingin_dikonversi == list_satuan_skala[6]:
       return (nilai_suhu - 32) * 11/80;
-    elif skala_yang_ingin_dikonversi == 'romer':
+    elif skala_yang_ingin_dikonversi == list_satuan_skala[7]:
       return (nilai_suhu - 32) * 7/24 + 7.5;
     else:
       return;
 
   # Mencari nilai konversi dari satuan Celcius
-  elif skala_yang_dipilih == 'celcius':
-    if skala_yang_ingin_dikonversi == 'kelvin':
+  elif skala_yang_dipilih == list_satuan_skala[2]:
+    if skala_yang_ingin_dikonversi == list_satuan_skala[0]:
       return nilai_suhu + 273.15;
-    elif skala_yang_ingin_dikonversi == 'fahrenheit':
+    elif skala_yang_ingin_dikonversi == list_satuan_skala[1]:
       return nilai_suhu * 9/5 + 32;
-    elif skala_yang_ingin_dikonversi == 'celcius':
+    elif skala_yang_ingin_dikonversi == list_satuan_skala[2]:
       return nilai_suhu;
-    elif skala_yang_ingin_dikonversi == 'reamur':
+    elif skala_yang_ingin_dikonversi == list_satuan_skala[3]:
       return nilai_suhu * 4/5;
-    elif skala_yang_ingin_dikonversi == 'rankine':
+    elif skala_yang_ingin_dikonversi == list_satuan_skala[4]:
       return (nilai_suhu + 273.15) * 9/5;
-    elif skala_yang_ingin_dikonversi == 'delisle':
+    elif skala_yang_ingin_dikonversi == list_satuan_skala[5]:
       return (100 - nilai_suhu) * 3/2;
-    elif skala_yang_ingin_dikonversi == 'newton':
+    elif skala_yang_ingin_dikonversi == list_satuan_skala[6]:
       return nilai_suhu * 33/100;
-    elif skala_yang_ingin_dikonversi == 'romer':
+    elif skala_yang_ingin_dikonversi == list_satuan_skala[7]:
       return nilai_suhu * 21/40 + 7.5;
     else:
       return;
 
   # Mencari nilai konversi dari satuan Reamur
-  elif skala_yang_dipilih == 'reamur':
-    if skala_yang_ingin_dikonversi == 'kelvin':
+  elif skala_yang_dipilih == list_satuan_skala[3]:
+    if skala_yang_ingin_dikonversi == list_satuan_skala[0]:
       return nilai_suhu * 5/4 + 273.15;
-    elif skala_yang_ingin_dikonversi == 'fahrenheit':
+    elif skala_yang_ingin_dikonversi == list_satuan_skala[1]:
       return nilai_suhu * 9/4 + 32;
-    elif skala_yang_ingin_dikonversi == 'celcius':
+    elif skala_yang_ingin_dikonversi == list_satuan_skala[2]:
       return nilai_suhu * 5/4;
-    elif skala_yang_ingin_dikonversi == 'reamur':
+    elif skala_yang_ingin_dikonversi == list_satuan_skala[3]:
       return nilai_suhu;
-    elif skala_yang_ingin_dikonversi == 'rankine':
+    elif skala_yang_ingin_dikonversi == list_satuan_skala[4]:
       return nilai_suhu * 9/4 + 491.67;
-    elif skala_yang_ingin_dikonversi == 'delisle':
+    elif skala_yang_ingin_dikonversi == list_satuan_skala[5]:
       return (80 - nilai_suhu) * 15/8;
-    elif skala_yang_ingin_dikonversi == 'newton':
+    elif skala_yang_ingin_dikonversi == list_satuan_skala[6]:
       return nilai_suhu * 33/80;
-    elif skala_yang_ingin_dikonversi == 'romer':
+    elif skala_yang_ingin_dikonversi == list_satuan_skala[7]:
       return nilai_suhu * 21/32 + 7.5;
     else:
       return;
       
   # Mencari nilai konversi dari satuan Rankine
-  elif skala_yang_dipilih == 'rankine':
-    if skala_yang_ingin_dikonversi == 'kelvin':
+  elif skala_yang_dipilih == list_satuan_skala[4]:
+    if skala_yang_ingin_dikonversi == list_satuan_skala[0]:
       return nilai_suhu * 5/9;
-    elif skala_yang_ingin_dikonversi == 'fahrenheit':
+    elif skala_yang_ingin_dikonversi == list_satuan_skala[1]:
       return nilai_suhu - 459.67;
-    elif skala_yang_ingin_dikonversi == 'celcius':
+    elif skala_yang_ingin_dikonversi == list_satuan_skala[2]:
       return (nilai_suhu - 491.67) * 5/9;
-    elif skala_yang_ingin_dikonversi == 'reamur':
+    elif skala_yang_ingin_dikonversi == list_satuan_skala[3]:
       return (nilai_suhu - 491.67) * 4/9;
-    elif skala_yang_ingin_dikonversi == 'rankine':
+    elif skala_yang_ingin_dikonversi == list_satuan_skala[4]:
       return nilai_suhu;
-    elif skala_yang_ingin_dikonversi == 'delisle':
+    elif skala_yang_ingin_dikonversi == list_satuan_skala[5]:
       return (671.67 - nilai_suhu) * 5/6;
-    elif skala_yang_ingin_dikonversi == 'newton':
+    elif skala_yang_ingin_dikonversi == list_satuan_skala[6]:
       return (nilai_suhu - 491.67) * 11/80;
-    elif skala_yang_ingin_dikonversi == 'romer':
+    elif skala_yang_ingin_dikonversi == list_satuan_skala[7]:
       return (nilai_suhu - 491.67) * 7/24 + 7.5;
     else:
       return;
   
   # Mencari nilai konversi dari satuan Delisle
-  elif skala_yang_dipilih == 'delisle':
-    if skala_yang_ingin_dikonversi == 'kelvin':
+  elif skala_yang_dipilih == list_satuan_skala[5]:
+    if skala_yang_ingin_dikonversi == list_satuan_skala[0]:
       return 373.15 - nilai_suhu * 2/3;
-    elif skala_yang_ingin_dikonversi == 'fahrenheit':
+    elif skala_yang_ingin_dikonversi == list_satuan_skala[1]:
       return 212 - nilai_suhu * 6/5;
-    elif skala_yang_ingin_dikonversi == 'celcius':
+    elif skala_yang_ingin_dikonversi == list_satuan_skala[2]:
       return 100 - nilai_suhu * 2/3;
-    elif skala_yang_ingin_dikonversi == 'reamur':
+    elif skala_yang_ingin_dikonversi == list_satuan_skala[3]:
       return 80 - nilai_suhu * 8/15;
-    elif skala_yang_ingin_dikonversi == 'rankine':
+    elif skala_yang_ingin_dikonversi == list_satuan_skala[4]:
       return 671.67 - nilai_suhu * 6/5;
-    elif skala_yang_ingin_dikonversi == 'delisle':
+    elif skala_yang_ingin_dikonversi == list_satuan_skala[5]:
       return nilai_suhu;
-    elif skala_yang_ingin_dikonversi == 'newton':
+    elif skala_yang_ingin_dikonversi == list_satuan_skala[6]:
       return 33 - nilai_suhu * 11/50;
-    elif skala_yang_ingin_dikonversi == 'romer':
+    elif skala_yang_ingin_dikonversi == list_satuan_skala[7]:
       return 60 - nilai_suhu * 7/20;
     else:
       return;
 
   # Mencari nilai konversi dari satuan Newton
-  elif skala_yang_dipilih == 'newton':
-    if skala_yang_ingin_dikonversi == 'kelvin':
+  elif skala_yang_dipilih == list_satuan_skala[6]:
+    if skala_yang_ingin_dikonversi == list_satuan_skala[0]:
       return nilai_suhu * 100/33 + 273.15;
-    elif skala_yang_ingin_dikonversi == 'fahrenheit':
+    elif skala_yang_ingin_dikonversi == list_satuan_skala[1]:
       return nilai_suhu * 60/11 + 32;
-    elif skala_yang_ingin_dikonversi == 'celcius':
+    elif skala_yang_ingin_dikonversi == list_satuan_skala[2]:
       return nilai_suhu * 100/33;
-    elif skala_yang_ingin_dikonversi == 'reamur':
+    elif skala_yang_ingin_dikonversi == list_satuan_skala[3]:
       return nilai_suhu * 80/33;
-    elif skala_yang_ingin_dikonversi == 'rankine':
+    elif skala_yang_ingin_dikonversi == list_satuan_skala[4]:
       return nilai_suhu * 60/11 + 491.67;
-    elif skala_yang_ingin_dikonversi == 'delisle':
+    elif skala_yang_ingin_dikonversi == list_satuan_skala[5]:
       return (33 - nilai_suhu) * 50/11;
-    elif skala_yang_ingin_dikonversi == 'newton':
+    elif skala_yang_ingin_dikonversi == list_satuan_skala[6]:
       return nilai_suhu;
-    elif skala_yang_ingin_dikonversi == 'romer':
+    elif skala_yang_ingin_dikonversi == list_satuan_skala[7]:
       return nilai_suhu * 35/22 + 7.5;
     else:
       return;
 
 # Mencari nilai konversi dari satuan Romer
-  elif skala_yang_dipilih == 'romer':
-    if skala_yang_ingin_dikonversi == 'kelvin':
+  elif skala_yang_dipilih == list_satuan_skala[7]:
+    if skala_yang_ingin_dikonversi == list_satuan_skala[0]:
       return (nilai_suhu - 7.5) * 40/21 + 273.15;
-    elif skala_yang_ingin_dikonversi == 'fahrenheit':
+    elif skala_yang_ingin_dikonversi == list_satuan_skala[1]:
       return (nilai_suhu - 7.5) * 24/7 + 32;
-    elif skala_yang_ingin_dikonversi == 'celcius':
+    elif skala_yang_ingin_dikonversi == list_satuan_skala[2]:
       return (nilai_suhu - 7.5) * 40/21;
-    elif skala_yang_ingin_dikonversi == 'reamur':
+    elif skala_yang_ingin_dikonversi == list_satuan_skala[3]:
       return (nilai_suhu - 7.5) * 32/21;
-    elif skala_yang_ingin_dikonversi == 'rankine':
+    elif skala_yang_ingin_dikonversi == list_satuan_skala[4]:
       return (nilai_suhu - 7.5) * 24/7 + 491.67;
-    elif skala_yang_ingin_dikonversi == 'delisle':
+    elif skala_yang_ingin_dikonversi == list_satuan_skala[5]:
       return (60 - nilai_suhu) * 20/7;
-    elif skala_yang_ingin_dikonversi == 'newton':
+    elif skala_yang_ingin_dikonversi == list_satuan_skala[6]:
       return (nilai_suhu - 7.5) * 22/35;
-    elif skala_yang_ingin_dikonversi == 'romer':
+    elif skala_yang_ingin_dikonversi == list_satuan_skala[7]:
       return nilai_suhu;
     else:
       return;
 
 #* Hasil Konversi di proses kedalam variabel hasil_konversi
-hasil_konversi = konversi_skala_suhu(skala_yang_dipilih, nilai_suhu, skala_yang_ingin_dikonversi);
+hasil_konversi = konversi_skala_suhu(list_satuan_skala[skala_yang_dipilih], nilai_suhu, list_satuan_skala[skala_yang_ingin_dikonversi]);
 
 # Menampilkan hasil
 #! Akan memvalidasi, jika input nama skala bernilai sama
 if skala_yang_dipilih == skala_yang_ingin_dikonversi :
   raise ValueError("Konversi tidak akan berlaku jika kedua nama skala suhu sama");
 else:
-  print(f"\nHasil konversi dari {nilai_suhu} {skala_yang_dipilih} ke {skala_yang_ingin_dikonversi} adalah {hasil_konversi}\n\n\n");
+  print(f"\nHasil konversi dari {nilai_suhu} {list_satuan_skala[skala_yang_dipilih]} ke {list_satuan_skala[skala_yang_ingin_dikonversi]} adalah {hasil_konversi}\n\n\n");
